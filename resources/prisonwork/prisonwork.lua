b125f606bbf37d736d0d89e8808a504ee02b1c54
Hunter Miller <contact@idevhunter.com>
2019-04-09 10:58:22 -0400

    add comments to prisonwork.lua

--- resources/prisonwork/prisonwork.lua
+++ resources/prisonwork/prisonwork.lua
@@ -1,33 +1,22 @@
-
+--[[
+FiveM / GTA Natives: https://runtime.fivem.net/doc/natives/
+]]
 Citizen.CreateThread(function()
   local hammering_lib = "amb@world_human_hammering@male@base" -- hammering with base
-  RequestAnimDict(hammering_lib)
-  while not HasAnimDictLoaded(hammering_lib) do
-    Citizen.Wait(100)
+  RequestAnimDict(hammering_lib) --get animation library from game
+  while not HasAnimDictLoaded(hammering_lib) do --if library does not exist
+    Citizen.Wait(100) 
   end
-  while true do
+  while true do --if library exists
     Citizen.Wait(0)
-
-
-
-    --[[
-    local location = vector3(1697.97,2683.93,45.56) --location of marker
-    local playerPed = PlayerPedId() -- get the local player ped
-    local pos = GetEntityCoords(playerPed) -- get the position of the local player ped
-    if location == pos.x, pos.y, pos.z then
-      TriggerEvent("chatMessage", "", { 0, 0, 0 }, "^*^2You've received $500 for making a license plate")
-    end]]
-    local areas = { {x = 1697.97, y = 2683.93, z = 45.56} }
-
-    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-
+    local areas = { {x = 1697.97, y = 2683.93, z = 45.56} } --set marker x,y,z coords
+    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true)) --set ped x,y,z coords
     for k, v in pairs(areas) do
-      distance = GetDistanceBetweenCoords(x, y, z, v.x, v.y, v.z, true)
+      distance = GetDistanceBetweenCoords(x, y, z, v.x, v.y, v.z, true) --get distance between ped coords and marker coords
       Citizen.Wait(100)
-      distance = math.ceil(distance)
-
-      if distance <= 1 then
-        --TriggerEvent("chatMessage", "", { 0, 0, 0 }, "Distance is : " .. distance)
+      distance = math.ceil(distance) --round up distance to whole int
+      if distance <= 1 then --check if distance between ped and marker is less than or equal to 1
+        --TriggerEvent("chatMessage", "", { 0, 0, 0 }, "Distance is : " .. distance) --for debug
         local Keys = {
           ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
           ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
@@ -39,14 +28,13 @@ Citizen.CreateThread(function()
           ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
           ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
         }
-        if IsControlPressed(1, Keys["G"]) then
-          TaskPlayAnim(GetPlayerPed(-1), hammering_lib, "base", 8.0, 8.0, - 1, 50, 0, true, true, true)
+        if IsControlPressed(1, Keys["G"]) then --press G to start job
+          TaskPlayAnim(GetPlayerPed(-1), hammering_lib, "base", 8.0, 8.0, - 1, 50, 0, true, true, true) --hammering emote with locked movement
           Citizen.Wait(3000)
           ClearPedTasks(GetPlayerPed(-1)) --reset ped emote
-          local platenum = math.floor(math.random() * 100000 + 1)
-          local remaining = math.floor(math.random(1000, 9999))
+          local platenum = math.floor(math.random() * 100000 + 1) --generate random 5 digit number for license plate
+          local remaining = math.floor(math.random(1000, 9999)) --generate random number between 1000 and 9999
           TriggerEvent("chatMessage", "", { 0, 0, 0 }, "^*^3ACTION: ^0License Plate Created : ^2[ " .. platenum .. " ]. ^r^0" .. remaining .. " license plates remaining..")
-
           --[[
           ADD MONEY TO PLAYERS BANK ONCE JOB IS COMPLETED
           ]]
