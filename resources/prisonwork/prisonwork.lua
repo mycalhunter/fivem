8e360be3ad3fcd890fec9a39537eea47361600e0
younggunsinc <45574262+younggunsinc@users.noreply.github.com>
2019-04-09 12:59:29 -0400

    Update prisonwork.lua

--- resources/prisonwork/prisonwork.lua
+++ resources/prisonwork/prisonwork.lua
@@ -5,7 +5,7 @@ Citizen.CreateThread(function()
   local hammering_lib = "amb@world_human_hammering@male@base" -- hammering with base
   RequestAnimDict(hammering_lib) --get animation library from game
   while not HasAnimDictLoaded(hammering_lib) do --if library does not exist
-    Citizen.Wait(100) 
+    Citizen.Wait(100)
   end
   while true do --if library exists
     Citizen.Wait(0)
@@ -19,11 +19,11 @@ Citizen.CreateThread(function()
         --TriggerEvent("chatMessage", "", { 0, 0, 0 }, "Distance is : " .. distance) --for debug
         local Keys = {
           ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-          ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, 
-          ["BACKSPACE"] = 177, ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, 
-          ["ENTER"] = 18, ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182, ["LEFTSHIFT"] = 21, ["Z"] = 20, 
-          ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81, ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
-          ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178, ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173, ["NENTER"] = 201, ["N4"] = 108, 
+          ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83,
+          ["BACKSPACE"] = 177, ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40,
+          ["ENTER"] = 18, ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182, ["LEFTSHIFT"] = 21, ["Z"] = 20,
+          ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81, ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+          ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178, ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173, ["NENTER"] = 201, ["N4"] = 108,
           ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
         }
         if IsControlPressed(1, Keys["G"]) then --press G to start job
@@ -37,12 +37,21 @@ Citizen.CreateThread(function()
           ADD MONEY TO PLAYERS BANK ONCE JOB IS COMPLETED
           ]]
           TriggerEvent("chatMessage", "", { 0, 0, 0 }, "^*^4SERVER: Before adding money")
-          Citizen.Wait(1500)
-          --essentialmode add money 
-          local user = exports.essentialmode:getUser()
-          local userMoney = user.getMoney()
-          userMoney = userMoney + user.addMoney(100)
-          user.displayMoney(userMoney)
+
+
+          --essentialmode add money
+          AddEventHandler('es:playerLoaded', function(source)
+            -- Get the current amount for the player
+            TriggerEvent('es:getPlayerFromId', source, function(user)
+              -- Activate the money for the current player
+              user:setMoney((user.money))
+              -- Send the player some information regarding the money
+              TriggerClientEvent('chatMessage', source, "SYSTEM", {187, 235, 42}, "Your money amount is: $" .. tonumber(user.money))
+            end)
+          end)
+
+
+
           TriggerEvent("chatMessage", "", { 0, 0, 0 }, "^*^4SERVER: After adding money")
         else
           ClearPedTasks(GetPlayerPed(-1))
