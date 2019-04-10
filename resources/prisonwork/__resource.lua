8c8bc34f87c8c105cce995893e3aea4f1af5efcb
Hunter Miller <contact@idevhunter.com>
2019-04-10 09:18:23 -0400

    update prisonwork resource

--- resources/prisonwork/__resource.lua
+++ resources/prisonwork/__resource.lua
@@ -2,17 +2,19 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description "Prison Jobs"
 
-dependency "essentialmode"
-dependency "[sesx]/essentialmode"
-
 client_scripts {
 	'prisonwork.lua',
-	'config.lua',
-	--'@essentialmode/server/player/wrappers.lua',
+	'teleport.lua',
+	'@essentialmode/server/player/wrappers.lua',
 	'shared.js',
 	'client.js'
 }
---[[server_scripts {
-	'shared.js',
-	'server/server.js'
-}]]--
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@essentialmode/server/player/wrappers.lua'
+}
+dependencies {
+	'es_extended',
+	'essentialmode',
+	'mysql-async'
+}
