5905b5279247b4d88a540a39bc7a1134968dbd15
younggunsinc <45574262+younggunsinc@users.noreply.github.com>
2019-04-15 19:47:40 -0400

    Update blips.lua

--- resources/blips/blips.lua
+++ resources/blips/blips.lua
@@ -8,14 +8,12 @@ end)
 
 local blips = {
 -- Example {title="", colour=, id=, x=, y=, z=},
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1629.76, y = 2563.87, z = 45.56, display = 4, scale = 1.0, shortRange = true, MarkerType = 99, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 25}, prompt = "Press ~y~E~s~ to repair electric panel"}, --electric repair
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1753.49, y = 2503.66, z = 45.57, display = 4, scale = 1.0, shortRange = true, MarkerType = 99, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 25}, prompt = "Press ~y~E~s~ to make license plate"}, --license plate
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1755.42, y = 2467.19, z = 55.14, display = 4, scale = 1.0, shortRange = true, MarkerType = 99, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 25}, prompt = "Press ~y~E~s~ to search AC Unit"} --hidden knife
+{title = "Prison Maintenance", colour = 1, id = 446, x = 1629.76, y = 2563.87, z = 45.56, display = 4, scale = 1.0, shortRange = true, MarkerType = 1, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to repair electric panel"}, --electric repair
+{title = "Prison Maintenance", colour = 1, id = 446, x = 1753.49, y = 2503.66, z = 45.57, display = 4, scale = 1.0, shortRange = true, MarkerType = 1, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to make license plate"}, --license plate
+{title = "Prison Maintenance", colour = 1, id = 446, x = 1755.42, y = 2467.19, z = 55.14, display = 4, scale = 1.0, shortRange = true, MarkerType = 1, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to search AC Unit"} --hidden knife
 }
 
-Citizen.CreateThread(function()
-  local playerx, playery, playerz = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-
+Citizen.CreateThread(function())
   for _, info in pairs(blips) do
     info.blip = AddBlipForCoord(info.x, info.y, info.z)
     SetBlipSprite(info.blip, info.id)
@@ -26,14 +24,24 @@ Citizen.CreateThread(function()
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentString(info.title)
     EndTextCommandSetBlipName(info.blip)
+  end --end for
 
+
+  for k, v in pairs(blips) do
+    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+    local distance = GetDistanceBetweenCoords(x, y, z, v.x, v.y, v.z, false)
+    distance = math.ceil(distance)
     --[[DRAW MARKER IF PLAYER IS WITHIN DRAWDISTANCE]]
-    if (GetDistanceBetweenCoords(playerx, playery, playerz, info.x, info.y, info.z, false) < info.DrawDistance) then
-      DrawMarker(info.MarkerType, info.x, info.y, info.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, info.MarkerSize.x, info.MarkerSize.y, info.MarkerSize.z - 0.0, info.MarkerColor.r, info.MarkerColor.g, info.MarkerColor.b, info.MarkerColor.a, 50, false, true, 2, false, false, false, false)
+    if distance < v.DrawDistance then
+      DrawMarker(1, v.x, v.y, v.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.MarkerSize.x, v.MarkerSize.y, v.MarkerSize.z, v.MarkerColor.r, v.MarkerColor.g, v.MarkerColor.b, 50, false, false, 2, false, false, false, false)
     end
     --[[DRAW MESSAGE IF PLAYER IS WITHIN MARKERSIZE.X]]
-    if (GetDistanceBetweenCoords(playerx, playery, playerz, info.x, info.y, info.z, false) < info.MarkerSize.x) then
-      TriggerEvent('esx:showNotification', s, info.prompt)
+    if GetDistanceBetweenCoords(x, y, z, v.x, v.y, v.z, false) < v.MarkerSize.x / 2 then
+      TriggerEvent('esx:showNotification', source, v.prompt)
     end
-  end
-end)
+  end --end for
+
+
+
+
+end) --end thread
