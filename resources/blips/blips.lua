b79931b7faab4f72d31efed994f645798f42bd0c
younggunsinc <45574262+younggunsinc@users.noreply.github.com>
2019-04-15 22:42:03 -0400

    Update blips.lua

--- resources/blips/blips.lua
+++ resources/blips/blips.lua
@@ -1,26 +1,16 @@
 ESX = nil
---[[GET ESX OBJ DATA --VERY IMPORTANT]]
-Citizen.CreateThread(function()
-  while ESX == nil do
-  TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-  Citizen.Wait(0)
-end
-end)
-
 local blips = {
--- Example {title="", colour=, id=, x=, y=, z=},
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1629.76, y = 2563.87, z = 45.56, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 100.0, markersize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to repair electric panel"}, --electric repair
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1753.49, y = 2503.66, z = 45.57, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 100.0, markersize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to make license plate"}, --license plate
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1755.42, y = 2467.19, z = 55.14, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 100.0, markersize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to search AC Unit"} --hidden knife
+  -- Example {title="", colour=, id=, x=, y=, z=},
+  {title = "Prison Maintenance", colour = 4, id = 446, x = 1629.76, y = 2563.87, z = 45.56, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 200.0, markersize = {x = 2, y = 2, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 255, a = 100}, prompt = "Press ~y~E~s~ to repair electric panel"}, --electric repair
+  {title = "Prison Maintenance1", colour = 4, id = 446, x = 1753.49, y = 2503.66, z = 45.57, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 200.0, markersize = {x = 2, y = 2, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 255, a = 100}, prompt = "Press ~y~E~s~ to make license plate"}, --license plate
+  {title = "Prison Maintenance2", colour = 4, id = 446, x = 1755.42, y = 2467.19, z = 55.14, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 200.0, markersize = {x = 2, y = 2, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 255, a = 100}, prompt = "Press ~y~E~s~ to search AC Unit"} --hidden knife
 }
-
-Citizen.CreateThread(function())
-
-
-
-
-
-  --[[THIS WORKS, DONT TOUCH]]
+function HelpText(text)
+  SetTextComponentFormat("STRING")
+  AddTextComponentString(text)
+  DisplayHelpTextFromStringLabel(0, 0, 0, - 1)
+end
+Citizen.CreateThread(function()
   --RENDER BLIP ON MAP
   for _, info in pairs(blips) do
     info.blip = AddBlipForCoord(info.x, info.y, info.z)
@@ -33,33 +23,46 @@ Citizen.CreateThread(function())
     AddTextComponentString(info.title)
     EndTextCommandSetBlipName(info.blip)
   end --end for
-
-
-
-
-
-
-  --[[THIS DOESN'T WORK, NEED TO FIX]]
-  --RENDER MARKER NEAR PLAYER
-  for _, v in pairs(blips) do
-    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true)) --get player position
-    local distance = GetDistanceBetweenCoords(x, y, z, v.x, v.y, v.z, false) --get distance between player and marker coords
-    --distance = math.ceil(distance)
-
-    if distance < v.drawdistance then
-      DrawMarker(v.markertype, v.x, v.y, v.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.markersize.x, v.markersize.y, v.markersize.z, v.markercolor.r, v.markercolor.g, v.markercolor.b, 50, false, false, 2, false, false, false, false)
-    end
-    if distance < v.markersize.x then
-      TriggerEvent('esx:showNotification', source, v.prompt)
-    end
-  end --end for
-
-
-
-
-
-
-
-
-
 end) --end thread
+
+
+
+
+--[[GET ESX OBJ DATA --VERY IMPORTANT]]
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(0)
+  end
+end)
+Citizen.CreateThread(function()
+    --[[THIS DOESN'T WORK, NEED TO FIX]]
+    --RENDER MARKER NEAR PLAYER
+
+
+    --[[  for k, v, tab in ipairs(blips) do
+        local markerX = v.x
+        --print(markerX) --only outputing first set in blips
+        local markerY = v.y
+        local markerZ = v.z
+        print(v.x, v.y, v.z)
+    -- tab is the table or subtable val resides in, tab[key] == val
+end]]
+
+for _, v in ipairs(blips) do
+  while true do
+    Wait(0)
+    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), false)) --get player position
+    local distance = GetDistanceBetweenCoords(x, y, z, v.x, v.y, v.z, true) --get distance between player and marker coords
+        --distance = math.ceil(distance)
+        print(v.drawdistance)
+        if (distance < v.drawdistance) then --if distance between player and marker coords is less than 100
+          DrawMarker(1, v.x, v.y, v.z + 2.0, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 2.0, 2.0, 2.0, 255, 255, 0, 50, false, true, 2, nil, nil, false)
+        end
+        if (distance <= 1) then --if distance between player and marker is less than 1 meter/unit
+          HelpText(v.prompt)
+          TriggerEvent("chatMessage", "", { 0, 0, 0 }, v.prompt)
+        end
+      end
+    end --end for
+end)
