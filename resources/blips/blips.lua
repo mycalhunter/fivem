6bbb765421a110b1701ce2a8b91dc9fcf9b233f1
Hunter Miller <contact@idevhunter.com>
2019-04-16 11:26:52 -0400

    Update blips.lua

--- resources/blips/blips.lua
+++ resources/blips/blips.lua
@@ -4,10 +4,6 @@ function HelpText(text)
   DisplayHelpTextFromStringLabel(0, 0, 0, - 1)
 end
 
-
-
-
-
 local blips = {
   -- Example {title="", colour=, id=, x=, y=, z=},
   {title = "Prison Maintenance", colour = 4, id = 446, x = 1629.76, y = 2563.87, z = 45.56, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 200.0, markersize = {x = 2, y = 2, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 255, a = 100}, prompt = "Press ~y~E~s~ to repair electric panel"}, --electric repair
@@ -17,32 +13,35 @@ local blips = {
 
 --RENDER BLIP ON MAP
 Citizen.CreateThread(function()
-  for _, info in pairs(blips) do
-    info.blip = AddBlipForCoord(info.x, info.y, info.z)
-    SetBlipSprite(info.blip, info.id)
-    SetBlipDisplay(info.blip, info.display)
-    SetBlipScale(info.blip, info.scale)
-    SetBlipColour(info.blip, info.colour)
-    SetBlipAsShortRange(info.blip, info.shortRange)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString(info.title)
-    EndTextCommandSetBlipName(info.blip)
-  end --end for
+  while (true) do
+    Wait(1) --1 gametick
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, info.display)
+      SetBlipScale(info.blip, info.scale)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, info.shortRange)
+      BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end --end for
+  end --end while
 end) --end thread
 
 --RENDER MARKER AT LOCATION
 Citizen.CreateThread(function()
-for _, v in ipairs(blips) do
-  while true do
-    Wait(0)
-    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true)) --get player position
-    local distance = Vdist2(x, y, z, v.x, v.y, v.z) --get distance between player and marker coords
+  while (true) do
+    Wait(1) --1 gametick
+    for _, v in ipairs(blips) do
+    local x, y, z = GetEntityCoords(GetPlayerPed(-1)) --get player position
+    local distance = Vdist(x, y, z, v.x, v.y, v.z) --get distance between player and marker coords
         if (distance < v.drawdistance) then --if distance between player and marker coords is less than 100
           DrawMarker(1, v.x, v.y, v.z + 2.0, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 2.0, 2.0, 2.0, 255, 255, 0, 50, false, true, 2, nil, nil, false)
         end
         if (distance <= 1) then --if distance between player and marker is less than 1 meter/unit
           HelpText(v.prompt)
         end
-      end
-    end --end for
+      end --end for
+    end --end while
 end) --end thread
