f09ddade29ebc9b4bc7d28bab8fe553a9e21fcb3
younggunsinc <45574262+younggunsinc@users.noreply.github.com>
2019-04-15 20:09:58 -0400

    Update blips.lua

--- resources/blips/blips.lua
+++ resources/blips/blips.lua
@@ -9,9 +9,9 @@ end)
 
 local blips = {
 -- Example {title="", colour=, id=, x=, y=, z=},
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1629.76, y = 2563.87, z = 45.56, display = 4, scale = 1.0, shortRange = true, MarkerType = 1, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to repair electric panel"}, --electric repair
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1753.49, y = 2503.66, z = 45.57, display = 4, scale = 1.0, shortRange = true, MarkerType = 1, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to make license plate"}, --license plate
-{title = "Prison Maintenance", colour = 1, id = 446, x = 1755.42, y = 2467.19, z = 55.14, display = 4, scale = 1.0, shortRange = true, MarkerType = 1, DrawDistance = 100.0, MarkerSize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, MarkerColor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to search AC Unit"} --hidden knife
+{title = "Prison Maintenance", colour = 1, id = 446, x = 1629.76, y = 2563.87, z = 45.56, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 100.0, markersize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to repair electric panel"}, --electric repair
+{title = "Prison Maintenance", colour = 1, id = 446, x = 1753.49, y = 2503.66, z = 45.57, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 100.0, markersize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to make license plate"}, --license plate
+{title = "Prison Maintenance", colour = 1, id = 446, x = 1755.42, y = 2467.19, z = 55.14, display = 4, scale = 1.0, shortRange = true, markertype = 1, drawdistance = 100.0, markersize = {x = 1, y = 1, z = 2}, BlipSprite = 446, EnterExitDelay = 0, EnterExitDelayMax = 600, markercolor = {r = 255, g = 0, b = 0, a = 255}, prompt = "Press ~y~E~s~ to search AC Unit"} --hidden knife
 }
 
 Citizen.CreateThread(function())
@@ -35,11 +35,11 @@ Citizen.CreateThread(function())
     local distance = GetDistanceBetweenCoords(x, y, z, v.x, v.y, v.z, false) --get distance between player and marker coords
     --distance = math.ceil(distance)
 
-    if distance < v.DrawDistance then
-      DrawMarker(1, v.x, v.y, v.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.MarkerSize.x, v.MarkerSize.y, v.MarkerSize.z, v.MarkerColor.r, v.MarkerColor.g, v.MarkerColor.b, 50, false, false, 2, false, false, false, false)
+    if distance < v.drawdistance then
+      DrawMarker(v.markertype, v.x, v.y, v.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.markersize.x, v.markersize.y, v.markersize.z, v.markercolor.r, v.markercolor.g, v.markercolor.b, 50, false, false, 2, false, false, false, false)
     end
 
-    if distance < v.MarkerSize.x then
+    if distance < v.markersize.x then
       TriggerEvent('esx:showNotification', source, v.prompt)
     end
   end --end for
