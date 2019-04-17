f69cdbcde19ff2864168a73d33f893ffa4def8bf
Hunter Miller <contact@idevhunter.com>
2019-04-17 10:26:20 -0400

    remove js

--- resources/prisonwork/__resource.lua
+++ resources/prisonwork/__resource.lua
@@ -2,29 +2,14 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description "Prison Jobs"
 
---ui_page('html/index.html')
-
---[[client_scripts {
-  'client/main.lua',
-  '@essentialmode/server/player/wrappers.lua',
-}
-server_scripts {
-  'server/main.lua',
-  '@mysql-async/lib/MySQL.lua',
-  '@essentialmode/server/player/wrappers.lua'
-}]]
-
 client_scripts {
-	'client/main.lua',
-  'js/shared.js',
-  'js/client.js'
+	'client/main.lua'
 }
 
 server_scripts {
 	'server/main.lua',
   '@mysql-async/lib/MySQL.lua'
 }
-
 
 dependencies {
   'es_extended',
