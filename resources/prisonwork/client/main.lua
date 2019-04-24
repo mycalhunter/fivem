fb871af5a210d0e997ed59ec6e163a890929ac2f
Hunter Miller <contact@idevhunter.com>
2019-04-24 16:01:27 -0400

    cleaning up prisonwork variables

--- resources/prisonwork/client/main.lua
+++ resources/prisonwork/client/main.lua
@@ -29,16 +29,21 @@ local active = true
 local vehicleList = { "Rusty Surfer", "Faggio", "Rusty Rebel", "Surfer", "Faggio Sport", "Faggio Mod", "Regina", "Youga Classic", "Ingot", "Picador", "Primo Custom", "Blista", "Asea", "Dilettante", "Futo", "Minivan", "Premier", "Stanier", "Stratum", "Primo", "Monkey Blista", "Rhapsody", "Rumpo", "Rumpo II", "Bodhi", "Taco Van", "Asterope", "Issi", "Panto", "Prairie", "Ratbike", "Rebel", "Sabre Turbo", "Rancher XL", "Intruder", "Youga", "Brioso R/A", "Blazer", "Blazer Sport", "Dune Buggy", "Glendale", "Sanchez", "Tornado", "Vigero", "Warrener", "Bobcat XL", "Fugitive", "Beejay XL", "Blista Compact", "Boxville", "Journey", "Penumbra", "Sabre GT", "Seminole", "Washington", "Issi Classic", "Buccaneer", "Vader", "Baller", "Bison", "Emperor", "Faction", "Granger", "Stretch", "Carbon RS", "Gaunlet", "Radius", "Buffalo", "Burrito", "Cavalcade", "Paradise", "Virgo", "Surge", "Dukes", "Fuslade", "Pigalle", "Akuma", "Hexer", "Bifta", "Clown Van", "Dominator", "Injection", "PCJ-600", "Buffalo S", "Manana", "Ruffian", "Sandking", "Nemesis", "Bagger", "Baller 2nd Gen", "XLS", "Daemon", "Rat Truck", "Landstalker", "Sovereign", "Tailgator", "Tampa", "Zion", "Double T", "Cheburek", "Custom Minivan", "Daemon DLC", "Gang Burrito", "Oracle", "Peyote", "Rocoto", "Schafter", "Zion Cabrio", "Lectro", "Oracle XS", "Custom Voodoo", "Enduro", "Sentinel", "Wolfsbane", "Esskey", "Custom Virgo", "Huntley S", "Sentinel XS", "Custom Faction", "Dubsta", "Jackal", "Mesa", "FCR1000", "Avarus", "Chino", "Cliffhanger", "Custom Moonbeam", "Super Diamond", "Tornado Custom", "Vortex", "Casco", "Chino Custom", "Diablous", "Exemplar", "Felon", "Hustler", "Kalahari", "Slamvan", "Sultan", "Nightblade", "FCR1000 Custom", "Alpha", "Blade", "Chimera", "Felon GT", "Furore GT", "Hotknife", "Innovation", "Windsor", "Bati 801", "Zombie Bobber", "Patriot", "Windsor Drop", "Tornado Hot Rod", "Cognoscenti Cabrio", "Zombie Chopper", "Tulip", "Diablous Custom", "Elegy", "F620", "Sentinel Classic", "Suiner", "Feltzer", "Gargoyle", "Surano", "Baller LE", "Bati 801RR", "Coquette Classic", "Hermes", "Lynx", "Romeo Hearse", "Rumpo3", "Schafter V12", "Slamvan Custom", "Banshee", "Coquette", "Rapid GT", "9F", "Comet", "Rapid GT Convertible", "9F Cabrio", "Baller LWB", "Comet Retro Custom", "Contender", "Guardian", "Spacedocker", "Raiden", "Schwartzer", "Bestia GTS", "Elegy Retro Custom", "Voltic", "Phoenix", "Carbonizzare", "Sprunk Buffalo", "Bullet", "Massacro", "Hakuchou", "Kamacho", "Raptor", "Verkierer", "Jester Classic", "Sultan RS", "Massacro (Racecar)", "Nightshade", "Specter V2", "Ellie", "Stinger", "Comet Safari", "Flash GT", "Karuma", "Itali GT", "ETRI", "Brawler", "Bubsta 6x6", "JB 700", "Omnis", "Stirling GT", "Stinger GT", "Toros", "Tropos", "Vapid Dominator GTX", "Vindicator", "Coquette BlackFin", "Tyrus", "Franken Strange", "Merryweather Mesa", "Infernus", "Roosevelt Valor", "Penetrator", "Drift Tampa", "Roosevelt Valor Custom", "Cheetah", "Sanctus", "Banshee 900R", "Z-Type", "Burgershot Stalion", "Cheetah Classic", "Rapid GT Classic", "Deluxo", "Manroe", "Entity XF", "Trophy Truck", "Hakuchou Drag", "Turismo R", "Zentorno", "Shotaro", "Pfister811", "Reaper", "FMJ", "XA 21", "Osiris", "Ruston", "Dune Truck", "T20", "Jester", "Vagner", "X80 Proto", "Jester (Racecar)", "Adder", "RE-7B", "Mamba", "American Dream", "American Dream V2", "Nero Custom" }
 local Keys = { ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246 }
 local x, y, z = table.unpack(GetEntityCoords(playerPed, true)) --set ped x,y,z coords
+local distance = 0
+local chance = 0
+local currentHealth = 0
+local platenum = 0
+local vehicleName = ""
 --REMOVE KNIFE AND TARGETS
 AddEventHandler("playerSpawned", function(spawn)
-  RemoveWeaponFromPed(playerPed, "WEAPON_KNIFE")
+  RemoveWeaponFromPed(playerPed, knife)
   SetPedCanBeTargetted(playerPed, false)
 end)
 --REQUEST ANIMATION LIBRARIES
-RequestAnimDict(hl)
-RequestAnimDict(wl)
-RequestAnimDict(el)
-RequestAnimDict(kl)
+RequestAnimDict(hl) --hammer lib
+RequestAnimDict(wl) --welding lib
+RequestAnimDict(el) --electrocute lib
+RequestAnimDict(kl) --kneel lib
 while not HasAnimDictLoaded(hl, wl, el, kl) do --if library does not exist
   Citizen.Wait(100)
 end
@@ -53,12 +58,12 @@ while true do --if library exists
       if IsControlPressed(1, Keys["E"]) then
         TaskPlayAnim(playerPed, wl, wl_a, 8.0, 8.0, 5000, 1, 1, true, true, true)
         TaskStartScenarioInPlace(playerPed, welding_prop, 0, false)
-        local chance = math.floor(math.random(1, 50))
+        chance = math.floor(math.random(1, 50))
         if chance <= 25 then
           TriggerServerEvent("prisonworkjob")
         elseif chance >= 26 then
           TaskPlayAnim(playerPed, el, el_a, 8.0, 8.0, - 1, 50, 0, true, true, true)
-          local currentHealth = GetEntityHealth(playerPed)
+          currentHealth = GetEntityHealth(playerPed)
           SetEntityHealth(playerPed, currentHealth - 20)
           exports.pNotify:SendNotification({text = "You've been shocked, be careful.", type = "info", timeout = 8000, layout = "centerRight"})
         end --end if chance
@@ -80,9 +85,9 @@ while true do --if library exists
       if IsControlPressed(1, Keys["E"]) then
         TaskPlayAnim(playerPed, hl, hl_a, 8.0, 8.0, - 1, 50, 0, true, true, true)
         TaskStartScenarioInPlace(playerPed, hammer_prop, 0, false)
-        local platenum = math.floor(math.random() * 100000 + 1)
-        local chance = math.floor(math.random(1, 264))
-        local vehicleName = vehicleList[chance]
+        platenum = math.floor(math.random() * 100000 + 1)
+        chance = math.floor(math.random(1, 264))
+        vehicleName = vehicleList[chance]
         if chance <= 15 then
           exports.pNotify:SendNotification({text = "License Plate created [ " .. platenum .. " ] for a ^0" .. vehicleName .. ".", type = "info", timeout = 8000, layout = "centerRight"})
           TriggerServerEvent("prisonworkjob")
@@ -107,7 +112,7 @@ while true do --if library exists
     if distance <= 1 then
       if IsControlPressed(1, Keys["E"]) then
         TaskPlayAnim(playerPed, kl, kl_a, 8.0, 8.0, - 1, 50, 0, true, true, true)
-        local chance = math.floor(math.random(1, 50))
+        chance = math.floor(math.random(1, 50))
         if chance <= 10 then
           GiveWeaponToPed(playerPed, knife, 20, false, false)
           exports.pNotify:SendNotification({text = "You found a hidden knife..", type = "info", timeout = 8000, layout = "centerRight"})
--- resources/prisonwork/server/main.lua
+++ resources/prisonwork/server/main.lua
@@ -1,15 +1,22 @@
 ESX = nil
+local xPlayers = 0
+local xPlayer = 0
+local playerName = ""
+local s = source
+local amount = 0
+local getBank = 0
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+
 RegisterServerEvent('prisonworkjob')
 AddEventHandler('prisonworkjob', function(job)
-  local xPlayers = ESX.GetPlayers()
+  xPlayers = ESX.GetPlayers()
   for i = 1, #xPlayers, 1 do
-    local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-    local playerName = xPlayer.name
+    xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+    playerName = xPlayer.name
     if xPlayer.job.name == 'unemployed' then
-      local s = source
-      local amount = 275
-      local getBank = xPlayer.getBank()
+      amount = 275
+      getBank = xPlayer.getBank()
       xPlayer.setBankBalance(getBank + amount)
       Citizen.Wait(1)
       TriggerClientEvent('pNotify:SendNotification', -1, {text = '~y~You have been paid $' .. amount .. ' for your work.~s~', type = "info", timeout = 8000, layout = "centerRight"})
