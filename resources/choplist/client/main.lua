4bbb8c178f91f0c679ff19902cf319f4fe386ace
Hunter Miller <contact@idevhunter.com>
2019-05-07 10:15:13 -0400

    Update main.lua

--- resources/choplist/client/main.lua
+++ resources/choplist/client/main.lua
@@ -2,8 +2,6 @@ Citizen.CreateThread(function()
   -----------------------------------------------------
   ------ VARIABLES THAT WILL CHANGE DURING A JOB ------
   -----------------------------------------------------
-  dropoff = { { x = 946.99, y = -1698.05, z = 30.09 } }
-  start = { { x = -93.28, y = 19.40, z = 71.48 } }
   --[[BLIP FOR JOB START -- HIDE IN PRODUCTION]]
   info = AddBlipForCoord(-93.28, 19.40, 71.48)
   SetBlipSprite(info, 238)
@@ -14,7 +12,7 @@ Citizen.CreateThread(function()
   BeginTextCommandSetBlipName("STRING")
   AddTextComponentString("Chop List")
   EndTextCommandSetBlipName(info)
-  --DRAW TEXT ON MARKER
+  --[[DRAW TEXT ON MARKER]]
   function drawText(text)
     SetTextFont(0)
     SetTextProportional(1)
@@ -28,21 +26,19 @@ Citizen.CreateThread(function()
     AddTextComponentString(text)
     DrawText(0.35, 0.85) --position
   end
-
+  --[[CREATE PED TO STAND AT PICKUP LOCATION]]
   local model = GetHashKey("a_m_y_mexthug_01")
   RequestModel(model)
   while not HasModelLoaded(model) do
     Wait(0)
   end
   local chopPed = CreatePed(26, model, - 93.28, 19.40, 71.48 - 1.0, 172.48, false, true)
-
-
   while true do
     Citizen.Wait(0)
     -----------------------------------------------------
     ---- VARIABLES THAT WILL NOT CHANGE DURING A JOB ----
     -----------------------------------------------------
-    playerPed = GetPlayerPed(-1)
+    playerPed = GetPlayerPed(-1) --get player
     check = 0
     vehicleList = {
       [1] = { vehicle = "Bison" },
@@ -63,15 +59,15 @@ Citizen.CreateThread(function()
       ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
       ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
     }
-    handoff = "handposes-0" -- hammering with base
-    RequestAnimDict(handoff) --hammer lib
+    handoff = "handposes-0" -- notepad lib
+    RequestAnimDict(handoff) --request notepad lib
     notepad = "p_notepad_01_s-0" --notepad prop
     tod = "CODE_HUMAN_MEDIC_TIME_OF_DEATH"
-    timer = 10000 --timer between available jobs
-    veh = GetVehiclePedIsIn(playerPed, false)
-    model = GetEntityModel(veh)
-    displaytext = GetDisplayNameFromVehicleModel(model)
-    name = GetLabelText(displaytext)
+    timer = 10000 --timer between available jobs, 1000 = 1 sec
+    veh = GetVehiclePedIsIn(playerPed, false) --get current vehicle player is in
+    model = GetEntityModel(veh) --get model of vehicle
+    displaytext = GetDisplayNameFromVehicleModel(model) --get display name of model
+    name = GetLabelText(displaytext) --get label text of display name
 
 
     --[[ PICKUP ORDER FOR VEHICLE ]]
@@ -89,13 +85,12 @@ Citizen.CreateThread(function()
           ClearPedTasksImmediately(playerPed)
           chance = math.floor(math.random(1, #vehicleList))
           exports.pNotify:SendNotification({text = "Deliver a <font color='#FFFF00'>" .. vehicleList[chance].vehicle .. "</font> to the storage unit.", type = "info", timeout = 8000, layout = "centerRight"})
-          Citizen.Wait(timer) -- wait 60 seconds until next vehicle is available for selection
+          Citizen.Wait(timer) -- wait X seconds until next vehicle assignment is available
         end -- key press
       end -- distance check
 
 
     --[[ DROP OFF AND VERIFY VEHICLE ]]
-
       local x, y, z = table.unpack(GetEntityCoords(playerPed, true)) --set ped x,y,z coords
       if (Vdist(x, y, z, 946.99, -1698.05, 30.09, false) < 100.0) then
         DrawMarker(1,  946.99, -1698.05, 30.09 - 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 4.0, 1.0 + 0.5, 255, 0, 0, 500, false, false, 2, false, false, false, false)
@@ -106,14 +101,14 @@ Citizen.CreateThread(function()
           if IsControlJustPressed(1, Keys["E"]) then
             if name == vehicleList[chance].vehicle then
               exports.pNotify:SendNotification({text = "You have delivered an " .. name .. ".", type = "info", timeout = 8000, layout = "centerRight"})
-              SetEntityAsMissionEntity(veh)
+              SetEntityAsMissionEntity(veh) --give vehicle to game
               Wait(1000)
-              DeleteEntity(veh)
+              DeleteEntity(veh) --delete vehicle
               Wait(1000)
-              TriggerServerEvent("choplistjob")
+              TriggerServerEvent("choplistjob") --pay for delivering correct vehicle
             else
               exports.pNotify:SendNotification({text = "Does this look like a <font color='#FFFF00'>" .. vehicleList[chance].vehicle .. "</font> to you? Get out here.. ", type = "info", timeout = 8000, layout = "centerRight"})
-            end -- end for loop
+            end -- end if
           end -- key press
         end -- is ped in vehicle
       end -- 4 distance check
